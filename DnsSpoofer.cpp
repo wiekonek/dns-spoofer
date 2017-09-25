d49fa3797fb804f0bee8ec16007d6158eb7d39aa
wiekonek <wiekonek@gmail.com>
2017-09-25 19:34:42 +0200

    Add reading basic data from dns message
    - install libudns-dev to get it works
    - reading of message resources and query still not resolved
    - print dns domain name

--- DnsSpoofer.cpp
+++ DnsSpoofer.cpp
@@ -5,12 +5,22 @@
 #include <libnet.h>
 #include "DnsSpoofer.h"
 
+#include <algorithm>
+
+using std::for_each;
+
+struct dnsquery {
+    char *qname;
+    char qtype[2];
+    char qclass[2];
+};
 
 DnsSpoofer::DnsSpoofer() {
 
 }
 
 void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes);
+vector<unsigned char *> get_domain_name(unsigned char *query_payload);
 
 void DnsSpoofer::start_spoofing(char *device) {
     struct bpf_program fp;
@@ -31,11 +41,29 @@ void DnsSpoofer::start_spoofing(char *device) {
         exit(EXIT_FAILURE);
     }
     pcap_loop(pcap, -1, handle_dns_packet, NULL);
+}
 
+vector<unsigned char *> get_domain_name(unsigned char *query_payload) {
+    vector<unsigned char*> domain_name;
+    for(int i = 0; ; ) {
+        int current_length = query_payload[i++];
+        if(current_length == 0) {
+            break;
+        }
+        else {
+            auto domain_segment = new unsigned char(current_length + 1);
+            domain_segment[current_length] = '\0';
+            for(int j = 0; j < current_length; j++) {
+                domain_segment[j] = query_payload[i++];
+            }
+            domain_name.push_back(domain_segment);
+        }
+    }
+    return domain_name;
 }
 
 void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes) {
-    unsigned char *dns = (unsigned char *)(bytes + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct libnet_udp_hdr));
+    auto dns = (unsigned char *)(bytes + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct libnet_udp_hdr));
 
     printf("========= DNS =========\n");
     printf("Transaction ID: 0x%04x\n", dns_qid(dns));
@@ -43,7 +71,16 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
     printf("Answer RRs: %i\n", dns_numan(dns));
     printf("Authority RRs: %i\n", dns_numns(dns));
     printf("Additional RRs: %i\n", dns_numar(dns));
+    printf("response/request 0x%02x\n", dns_qr(dns));
 
-    cout << "data: " << dns_payload(dns) << endl;
+    if(dns_qr(dns) > 0) {
+        printf("We caught request query!\n");
+        auto domain_name_segments = get_domain_name(dns_payload(dns));
+        for(auto segment: domain_name_segments) {
+            printf("%s.", segment);
+        }
+        cout <<  endl;
+    }
+
     cout << "=======================" << endl << endl;
 }
--- DnsSpoofer.h
+++ DnsSpoofer.h
@@ -4,15 +4,18 @@
 static const char *const FILTER_DNS = "udp and port 53";
 
 #include <iostream>
+#include <vector>
+
 #include <net/if.h>
 #include <pcap/pcap.h>
-#include<linux/if_ether.h>
-#include<netinet/ip.h>    //Provides declarations for ip header
-
+#include <linux/if_ether.h>
+#include <netinet/ip.h>    //Provides declarations for ip header
 
 
 using std::cout;
 using std::endl;
+using std::vector;
+
 
 class DnsSpoofer {
 public:
